a107ffff9701c107194750cbf84c8ccfcddca7c7
Nicola Piga <nicolapiga@gmail.com>
2020-09-22 16:49:45 +0000

    Avoid forcing GPU id within ae/ae_train.py

--- auto_pose/ae/ae_train.py
+++ auto_pose/ae/ae_train.py
@@ -8,6 +8,8 @@ import shutil
 import cv2
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ["CUDA_DEVICE_ORDER"] = 'PCI_BUS_ID'
+# os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 import progressbar
 import tensorflow as tf
 
